85791ec403ccd2b734c9736be9999fd0f35a8aee
Skrish02 <sskrish786@gmail.com>
2023-01-24 18:18:31 +0530

    Commit

--- FrontEnd/resume/src/app/app.module.ts
+++ FrontEnd/resume/src/app/app.module.ts
@@ -4,19 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavbarComponent } from './component/navbar/navbar.component';
-import { FooterComponent } from './component/footer/footer.component';
+import { NavbarComponent } from './Component/navbar/navbar.component';
+import { FooterComponent } from './Component/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgbModule,
     NavbarComponent,
     FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
